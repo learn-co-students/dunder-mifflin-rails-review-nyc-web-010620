8720922accf6f066b71f34886e7f319ce7815352
edhernandez04 <mchh.004@gmail.com>
2020-02-06 13:05:18 -0500

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :first_name, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :dogs
   resources :employees
+
+  post "/dogs/:id/sort", to: "dogs#sort_it"
+  get "/dogs/:id/sort", to: "dogs#sort_it", as: "sort"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
