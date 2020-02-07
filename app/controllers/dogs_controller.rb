1cd6e55c05b5feadd74399891ec9d8468998f4ef
github username <github email address>
2020-02-06 22:22:43 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+    
+    def index 
+        @dogs = Dog.all
+    end
+
+    def sort
+        @dogs = Dog.all
+    end
+
+    def show 
+        @dog = Dog.find_by_id(params[:id])
+    end
 end
