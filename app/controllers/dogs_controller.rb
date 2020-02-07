017085638add82aadce9644dd0e92fee49a005a9
Anil Gurung <anil_grg@hotmail.com>
2020-02-06 22:03:51 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,21 @@
 class DogsController < ApplicationController
+    
+    def index
+
+    @dogs = Dog.all
+
+    end
+
+    def show
+
+    @dog = Dog.find(params[:id])
+
+    end
+
+    def new 
+
+    @dog = Dog.new
+
+    end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,52 @@
 class EmployeesController < ApplicationController
+
+    def index
+    
+        @employees = Employee.all
+
+    end
+
+
+
+    def new
+
+        @employee = Employee.new
+
+    end
+
+
+
+    def show
+
+        @employee = Employee.find(params[:id])
+
+    end
+
+    def create
+
+        @employee = Employee.new(employee_params)
+        if @employee.save
+          flash[:message] = "employee successfully created"
+          redirect_to @employee
+        else
+          flash.now[:message] = @employee.errors.full_messages[0]
+          render :new
+        end
+    end
+
+
+    
+   
+
+private
+
+    def employee_params
+     
+        params.require(:employee).permit(:first_name,:last_name,:alias,:title,:office,:img_url)
+
+    end
+
+
+
+
 end
